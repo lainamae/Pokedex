afbd41baecaca54a11e3dace5822d9babb4c6c09
lainamae <64325945+lainamae@users.noreply.github.com>
2021-05-20 13:54:12 -0400

    pokemonList now in IIFE, added add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
-var pokemonList = [
-  {
-    name: "jolteon",
-    types: "electric",
-    height: 0.8,
-  },
-  {
-    name: "charhander",
-    types: "fire",
-    height: 0.6,
-  },
-  {
-    name: "clefable",
-    types: "fairy",
-    height: 1.3,
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [    {
+        name: "jolteon",
+        types: "electric",
+        height: 0.8,
+      },
+      {
+        name: "charhander",
+        types: "fire",
+        height: 0.6,
+      },
+      {
+        name: "clefable",
+        types: "fairy",
+        height: 1.3,
+      }
+    ];
 
-pokemonList.forEach(function (pokemon) {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+let pokemonData = pokemonRepository.getAll();
+pokemonData.forEach(function (pokemon) {
   document.write(`<div><h2>${pokemon.name}</h2><p>${pokemon.types}</p><p>${pokemon.height}</p>
   </div>`);
 });
